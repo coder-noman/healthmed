136492f67534c240cd58be8a169a0470c114de38
coder-noman <abdhullaalnoman25@gmail.com>
2021-10-18 17:50:42 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSg-0oS6pHfJnDcsk46aPRLesszvmkYR4",
+  authDomain: "healthmade-e9598.firebaseapp.com",
+  projectId: "healthmade-e9598",
+  storageBucket: "healthmade-e9598.appspot.com",
+  messagingSenderId: "47724365184",
+  appId: "1:47724365184:web:64d66ec7276f47a8c26d65"
+};
+
+  export default firebaseConfig;
